c108dbae6bdd8df8080fb190695bed5c262bead8
Rim Sleimi <rim.sleimi@etudiant-fst.utm.tn>
2023-03-15 10:39:11 +0100

    add home.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import timelaps
+from apps import timelaps, Home
 
 st.set_page_config(layout="wide")
 
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 apps = MultiApp()
 
 # Add all your application here
-
+apps.add_app("Contact", Home.app)
 apps.add_app("Time Laps", timelaps.app)
 
 
